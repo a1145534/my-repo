16a16171585e5c5b1c0468d6214bfd96cb17a06e
a1145534 <a1145534@mail.nuk.edu.tw>
2026-04-10 10:51:29 +0000

    Hashmat the Brave Warrior

---
+++ Problem/UVa_10055.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int a,b;
+    while(cin>>a>>b){
+        if(a>b){
+            cout<<a-b<<endl;
+        }
+        else
+            cout<<b-a<<endl;
+    } 
+}
